ca63602eef3113fff6e3a144c594668eed85c1fd
Kiin <144398832+kientrung567@users.noreply.github.com>
2025-03-17 16:29:06 +0700

    Add files via upload

---
+++ project/submissions/progress 1/ReadData.py
@@ -0,0 +1,69 @@
+import redis
+import pandas as pd
+import gc
+
+# Kết nối tới Redis
+redis_host = "192.168.126.131"  # Địa chỉ IP của Ubuntu
+redis_port = 6379  # Cổng Redis mặc định
+
+r = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
+
+# ---------------------- #
+#  LƯU THÔNG TIN ANIME  #
+# ---------------------- #
+
+print("Đang đọc file anime-dataset-2023.csv ...")
+chunk_size = 50000  # Đọc file theo từng phần nhỏ
+for chunk in pd.read_csv("anime-dataset-2023.csv", chunksize=chunk_size):
+    data = chunk.to_dict(orient="records")  # Tăng tốc so với iterrows()
+
+    for row in data:
+        # Lưu thông tin Anime vào Redis dưới dạng Hash với Key là anime_id
+        anime_id = row["anime_id"]
+        r.hset(f"anime:{anime_id}", mapping=row)
+
+print("Hoàn thành lưu thông tin anime vào Redis!")
+del chunk, data
+# Giải phóng bộ nhớ
+gc.collect()
+
+# -------------------------------   #
+#  LƯU THÔNG TIN CÁ NHÂN NGƯỜI DÙNG #
+# -------------------------------   #
+print("Đang đọc file users-details-2023.csv ...")
+chunk_size = 50000
+for chunk in pd.read_csv("users-details-2023.csv", chunksize=chunk_size):
+    data = chunk.to_dict(orient="records")
+
+    for row in data:
+        # Lưu thông tin cá nhân người dùng vào Redis dưới dạng Hash với Key là user_id
+        user_id = row["Mal ID"]
+        r.hset(f"user:{user_id}", mapping=row)
+
+print("Hoàn thành lưu thông tin người dùng vào Redis!")
+del chunk, data
+# Giải phóng bộ nhớ
+gc.collect()
+
+# ------------------------------ #
+#  LƯU ĐIỂM ĐÁNH GIÁ NGƯỜI DÙNG  #
+# ------------------------------ #
+
+# print("Đang đọc file users-score-2023.csv ...")
+# chunk_size = 100000  # Đọc 100,000 dòng mỗi lần để tăng tốc
+# for chunk in pd.read_csv("users-score-2023.csv", chunksize=chunk_size):
+#     #    chunk = chunk.fillna("")  # Xử lý NaN
+#     data = chunk.to_dict(orient="records")
+
+#     for row in data:
+#         user_id = row["user_id"]
+#         anime_id = row["anime_id"]
+#         rating = row["rating"]
+#         # Lưu giá trị rating dưới dạng String trong Redis với Key là {user_id}_{anime_id}
+#         r.set(f"{user_id}_{anime_id}", rating)
+# print("Hoàn thành lưu điểm đánh giá vào Redis!")
+# del chunk, data
+# # Giải phóng bộ nhớ
+# gc.collect()
+
+print("Dữ liệu đã được lưu vào Redis thành công!")
